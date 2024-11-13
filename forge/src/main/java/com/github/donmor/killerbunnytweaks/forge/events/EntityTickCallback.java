845032cf431167dff1668f64ec42030df7829684
donmor <donmor3000@hotmail.com>
2024-11-13 10:35:29 +0800

    Remove unused stuff; add comments; minor tweaks

--- forge/src/main/java/com/github/donmor/killerbunnytweaks/forge/events/EntityTickCallback.java
+++ forge/src/main/java/com/github/donmor/killerbunnytweaks/forge/events/EntityTickCallback.java
@@ -2,11 +2,8 @@ package com.github.donmor.killerbunnytweaks.forge.events;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingTickEvent;
 import net.minecraft.world.entity.Entity;
-
-import javax.annotation.Nullable;
 
 import com.github.donmor.killerbunnytweaks.KBTEvents;
 
@@ -14,10 +11,7 @@ import com.github.donmor.killerbunnytweaks.KBTEvents;
 public class EntityTickCallback {
     @SubscribeEvent
     public static void onEntityTick(LivingTickEvent event) {
-        execute(event, event.getEntity());
-    }
-
-    private static void execute(@Nullable Event event, Entity entity) {
+        Entity entity = event.getEntity();
         if (entity == null)
             return;
         KBTEvents.OnEntityTick(entity);
